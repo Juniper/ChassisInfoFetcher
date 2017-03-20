d027380aa07162e568534a407cdb401a603ff403
mgospodinov <mgospodinov@juniper.net>
2017-03-20 16:27:45 +0100

    Remove test

--- test.py
+++
@@ -1,7 +0,0 @@
-import json
-import re
-
-with open("commands/MX_12.txt", "r") as data_file:
-    commandSettings = json.load(data_file)
-for item in commandSettings["commandList"]:
-	print re.sub('[|]', '"|"', item)
