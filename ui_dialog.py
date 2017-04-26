73efe03322cb5bc4962982f2ff8fcab786ea1663
mgospodinov <mgospodinov@juniper.net>
2017-04-26 12:00:51 +0200

    Change License to BSD 3-clause

--- ui_dialog.py
+++ ui_dialog.py
@@ -2,12 +2,23 @@
 #!/usr/bin/env python
 # <*******************
 # 
-# Copyright 2017 Juniper Networks, Inc. All rights reserved.
-# Licensed under the Juniper Networks Script Software License (the "License").
-# You may not use this script file except in compliance with the License, which is located at
-# http://www.juniper.net/support/legal/scriptlicense/
-# Unless required by applicable law or otherwise agreed to in writing by the parties, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   Copyright (c) 2017 Juniper Networks . All rights reserved.
+#   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+#   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+#   2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the 
+#   documentation and/or other materials provided with the distribution.
+#
+#   3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this 
+#   software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+#   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+#   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+#   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+#   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+#   EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # 
 # *******************>
 
