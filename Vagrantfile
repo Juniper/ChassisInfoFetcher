c0b5534f89119fb5d6a88580ddd34253203d28f9
Mitko Gospodinov <mgospodinov@juniper.net>
2018-06-21 16:41:08 +0200

    Change vagrant configuration

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,11 @@
 Vagrant.configure("2") do |config|
-	config.vm.box = "ubuntu/trusty64"
+	config.vm.box = "ubuntu/trusty32”
+	config.vm.boot_timeout = 900
 	config.vm.provision :shell, path: "bootstrap.sh"
-	config.vm.network "private_network", type: :dhcp
+	config.vm.provider “virtualbox” do |vb|
+		vb.customize [“modifyvm”, :id, “—-nictype”, “Am79C973”]
+	end
+	config.vm.network “public_network”
 	config.vm.synced_folder “C:\ChassisInfoFetcher\CIF", "/home/ubuntu/ChassisInfoFetcher"
 
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,10 @@
 apt-get update
 sudo apt-get install -y python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev git
 sudo apt-get --no-install-recommends install virtualbox-guest-utils
-sudo pip install --upgrade pip
-sudo pip install junos-eznc
+sudo apt-get install python-paramiko
+sudo pip install --upgrade setuptools
+sudo pip install --upgrade junos-eznc
 sudo pip install urwid
+sudo pip install enum
 git clone https://github.com/Juniper/py-space-platform.git
 sudo pip install ./py-space-platform
